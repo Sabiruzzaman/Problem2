27539e2739663d002bbb0bc68e27ea04d8617765
Me <sabiruzzaman16@gmail.com>
2021-01-27 16:04:22 +0600

    Problem2 update...

--- app/src/main/java/com/green_station_20/problem2/MainActivity.java
+++ app/src/main/java/com/green_station_20/problem2/MainActivity.java
@@ -13,11 +13,15 @@ import android.widget.Toast;
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -39,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 JsonRequest();
             }
         });
@@ -49,23 +54,24 @@ public class MainActivity extends AppCompatActivity {
 
         String URL = "https://alhasan.dev/interns/services/member-login.php";
 
-        String mobileNumber = mobileNumberEt.getText().toString();
-        String password = passwordEt.getText().toString();
-
-        mobileNumberEt.setText("");
-        passwordEt.setText("");
 
         StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
 
-                Toast.makeText(MainActivity.this, "" + response, Toast.LENGTH_SHORT).show();
-                //   Log.d("m", "onResponse: "+response);
+                /* JSONObject jsonObject = new JSONObject(response);*/
+                // Toast.makeText(MainActivity.this, "" + jsonObject, Toast.LENGTH_SHORT).show();
+                Log.d("m", "onResponse: " + response);
+
+
                 Intent intent = new Intent(MainActivity.this, SuccessActivity.class);
                 intent.putExtra("msg", response.toString());
                 startActivity(intent);
 
+
             }
+
+
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
@@ -75,16 +81,17 @@ public class MainActivity extends AppCompatActivity {
             }
         }) {
             @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
+            protected Map<String, String> getParams() {
 
-                Map<String, String> Params = new HashMap<>();
-                Params.put("mobile", mobileNumber);
-                Params.put("password", password);
-                return Params;
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("mobile", "01234567890");
+                params.put("password", "123456");
+
+                return params;
             }
         };
-
-        Volley.newRequestQueue(this).add(stringRequest);
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        queue.add(stringRequest);
 
     }
 }
